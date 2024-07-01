506fc562616952bf45e282753e6d6f6e388c97f6
Yohanis Tadese <127344303+yohanis-tadese@users.noreply.github.com>
2024-07-01 08:32:53 +0300

    Update Certeficate.jsx
    changes

--- src/Components/certeficate/Certeficate.jsx
+++ src/Components/certeficate/Certeficate.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import CertificateCard from "./CerteficateCard";
-import CerteficateOne from "../../assets/certeficate/certeficate-1.jpg";
-import CerteficateTwo from "../../assets/certeficate/certeficate-2.jpg";
-import CerteficateThree from "../../assets/certeficate/certeficate-3.jpg";
-import CerteficateFour from "../../assets/certeficate/certeficate-4.jpg";
-import CerteficateFive from "../../assets/certeficate/certeficate-5.jpg";
+import CerteficateOne from "../../assets/certeficate-1.jpg";
+import CerteficateTwo from "../../assets/certeficate-2.jpg";
+import CerteficateThree from "../../assets/certeficate-3.jpg";
+import CerteficateFour from "../../assets/certeficate-4.jpg";
+import CerteficateFive from "../../assets/certeficate-5.jpg";
+import CerteficateSix from "../../assets/certeficate-6.jpg";
 
 const cardClasses = "w-80 bg-white dark:bg-zinc-900 p-4 rounded-lg shadow-lg";
 const containerClasses =
@@ -27,6 +28,7 @@ const Certeficate = () => {
         <CertificateCard src={CerteficateThree} />
         <CertificateCard src={CerteficateFour} />
         <CertificateCard src={CerteficateFive} />
+        <CertificateCard src={CerteficateSix} />
       </div>
     </div>
   );
